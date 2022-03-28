1e2b5775e3d743800d6d50fc7b0880608974648f
CloudyLondon <jovialcty@gmail.com>
2022-03-27 17:58:11 -0700

    init code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Project from "./components/Project";
+import Footer from "./components/Footer";
+
+function App() {
+  return <div className="App"> //TODO: write html, routes and components</div>;
+}
+
+export default App;
